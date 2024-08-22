993e2cab4612ee05e1e24bdd6b94462bf0ec9f1e
noman <noman121998@gmail.com>
2024-08-22 15:27:06 +0200

    added PutMappting annotation on updateStudent method. and it's business logic on service class with annotaion Transactional

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,5 +40,26 @@ public class StudentService {
             throw new IllegalStateException("no student found");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException("student with" + studentId + "doesn't exsist"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email taken");
+            }
+
+            student.setEmail(email);
+        }
+
     }
 }
